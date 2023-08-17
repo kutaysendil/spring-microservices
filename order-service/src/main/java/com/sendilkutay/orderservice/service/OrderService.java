45464a48f86716155e394d8bebec1416a876d301
kutaysendil <sendil.kutay@gmail.com>
2023-08-18 01:42:52 +0300

    added spring web flux and connection services

--- order-service/src/main/java/com/sendilkutay/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/sendilkutay/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.sendilkutay.orderservice.service;
 
+import com.sendilkutay.orderservice.dto.InventoryResponse;
 import com.sendilkutay.orderservice.dto.OrderLineItemsDto;
 import com.sendilkutay.orderservice.dto.OrderRequest;
 import com.sendilkutay.orderservice.model.Order;
@@ -9,7 +10,9 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -19,7 +22,9 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    public void placeOrder(OrderRequest orderRequest) {
+    private final WebClient webClient;
+
+    public void placeOrder(OrderRequest orderRequest) throws IllegalAccessException {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
@@ -30,8 +35,24 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItems);
 
+        List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
+
         // Call inventory service and place order if product is in stock
-        orderRepository.save(order);
+
+        InventoryResponse[] inventoryResponses = webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean result = Arrays.stream(inventoryResponses).allMatch(InventoryResponse::getIsInStock);
+
+        if (Boolean.TRUE.equals(result)) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalAccessException("Product is not in stock");
+        }
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderItem) {
